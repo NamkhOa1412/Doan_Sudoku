3630e510f9c92ecb46b87b7525ccf12c58193b89
NamkhOa1412 <namkhoa0102@gmail.com>
2024-06-13 13:55:44 +0700

    fix UI

--- sudoku/lib/sudoku_widget/widget/sudoku_grid.dart
+++ sudoku/lib/sudoku_widget/widget/sudoku_grid.dart
@@ -16,12 +16,26 @@ class SudokuGrid extends StatelessWidget {
       itemBuilder: (context, index) {
         int row = index ~/ 9;
         int col = index % 9;
-        return SudokuCell(
-          value: grid[row][col],
-          isEditable: editableCells[row][col],
-          onChanged: (newValue) {
-            grid[row][col] = newValue;
-          },
+        double topBorder = (row % 3 == 0) ? 2.0 : 0.5;
+        double leftBorder = (col % 3 == 0) ? 2.0 : 0.5;
+        double rightBorder = (col == 8) ? 2.0 : 0.5;
+        double bottomBorder = (row == 8) ? 2.0 : 0.5;
+        return Container(
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(width: topBorder, color: Colors.black),
+              left: BorderSide(width: leftBorder, color: Colors.black),
+              right: BorderSide(width: rightBorder, color: Colors.black),
+              bottom: BorderSide(width: bottomBorder, color: Colors.black),
+            ),
+          ),
+          child: SudokuCell(
+            value: grid[row][col],
+            isEditable: editableCells[row][col],
+            onChanged: (newValue) {
+              grid[row][col] = newValue;
+            },
+          ),
         );
       },
       itemCount: 81,
